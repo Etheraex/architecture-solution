1e3a5e66339cb04f8d3c84e04f53b72644a9894e
Etheraex <mladjaether@gmail.com>
2026-06-27 15:45:41 +0200

    Added /token and /verify test case

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -1,28 +1,46 @@
 use serde_json::json;
-use std::thread;
+use std::{env, thread};
 use std::time::Duration;
 
 mod util;
 mod fix_generator;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let host = util::get_host_url();
-    let count = util::get_number_of_items_to_post();
+mod action;
+use action::Action;
 
-    let token_url = format!("http://{host}/token");
-    let fix_url = format!("http://{host}/fix");
+const HOST: &str = "http://localhost:80";
+
+const TOKEN_URL: &str = "/token";
+const VERIFY_TOKEN_URL: &str = "/verify";
+const FIX_URL: &str = "/fix";
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let action = match env::args().nth(1) {
+        Some(arg) => Action::from_args(&arg)?,
+        None => Action::None
+    };
+
+    match action {
+        Action::EndToEnd => run_end_to_end(),
+        Action::Auth => run_auth(),
+        Action::None => Ok(()),
+    }
+}
+
+fn run_end_to_end() -> Result<(), Box<dyn std::error::Error>> {
+    let count = util::get_number_of_items_to_post();
 
     let client = reqwest::blocking::Client::new();
 
-    let token = util::get_token_str(&client, token_url);
+    let token = util::get_token_string(&client, format!("{HOST}{TOKEN_URL}"))?;
     println!("Token successfully received: {token}");
 
-    println!("Firing {count} FIX messages at {fix_url} (1s interval)...\n");
+    println!("Firing {count} FIX messages at {FIX_URL} (1s interval)...\n");
 
     for seq in 1..=count {
         let msg = fix_generator::compose_fix(seq);
 
-        match client.post(&fix_url)
+        match client.post(format!("{HOST}{FIX_URL}"))
                 .bearer_auth(&token)
                 .json(&json!({"message": msg}))
                 .send() {
@@ -38,13 +56,35 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 eprintln!("[{seq}/{count}] -> request failed: {e}");
             }
         }
-
+    
         if seq < count {
             thread::sleep(Duration::from_secs(1));
         }
     }
 
     println!("Done.");
+
+    Ok(())
+}
+
+fn run_auth() -> Result<(), Box<dyn std::error::Error>> {
+    let client = reqwest::blocking::Client::new();
+
+    test_good_token(&client)?;
+
+    Ok(())
+}
+
+fn test_good_token(client: &reqwest::blocking::Client) -> Result<(), Box<dyn std::error::Error>> {
+    let token = util::get_token_string(client, format!("{HOST}{TOKEN_URL}"))?;
+
+    match client.get(format!("{HOST}{VERIFY_TOKEN_URL}"))
+            .bearer_auth(token)
+            .send() {
+        Ok(resp) if resp.status().is_success() => println!("Token verified!"),
+        Ok(resp) => println!("Bad token: HTTP {}", resp.status()),
+        Err(e) => println!("Bad token: {e}"),
+    }
 
     Ok(())
 }
--- tests/src/util.rs
+++ tests/src/util.rs
@@ -1,36 +1,23 @@
 use std::env;
 use serde::Deserialize;
 
-pub fn get_host_url() -> String {
-    env::args()
-        .nth(2)
-        .unwrap_or_else(|| "localhost:80".to_string())
-}
-
 pub fn get_number_of_items_to_post() -> usize {
     env::args()
-        .nth(1)
+        .nth(2)
         .and_then(|s| s.parse().ok())
         .unwrap_or(5)
 }
 
-pub fn get_token_str(client: &reqwest::blocking::Client, token_url: String) -> String {
+pub fn get_token_string(client: &reqwest::blocking::Client, token_url: String)
+        -> Result<String, Box<dyn std::error::Error>> {
 
     #[derive(Deserialize)]
     struct TokenResponse {
         token: String,
     }
 
-    match client.get(token_url).send() {
-        Ok(response) => {
-            let token_response: TokenResponse = response.json().unwrap_or_else(|error| {
-                panic!("Could not deserialize TokenResponse: {error}");
-            });
+    let response = client.get(token_url).send()?;
+    let token_response: TokenResponse = response.json()?;
 
-            token_response.token
-        }
-        Err (e) => {
-            panic!("Error fetching token: {e}");
-        }
-    }
+    Ok(token_response.token)
 }
